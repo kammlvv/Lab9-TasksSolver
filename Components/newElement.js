2b6269e7efec760ba72ac7104456b13201851af8
kammlvv <88438455+kammlvv@users.noreply.github.com>
2022-04-11 02:00:54 +0600

    add something in main

---
+++ Components/newElement.js
@@ -0,0 +1,8 @@
+function newElement() {
+  function close1(){
+    document.querySelector(".subjectsBox").style.display = "none"; 
+  } function open1(){
+    document.querySelector(".subjectsBox").style.display = "block";
+  }
+}
+export default newElement;
